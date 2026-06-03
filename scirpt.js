165412e87695e9408903344259f5caf3fdbda726
ghathaantarigan-cmyk <ghathaantarigan@gmail.com>
2026-06-03 20:16:37 +0700

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -1,6 +1,17 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
+const menuButton = document.getElementById('menu-button');
+const navbarMenu = document.getElementById('navbar-menu');
 
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
+// Membuka dan menutup navbar menu saat tombol hamburger di-klik
+menuButton.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+    menuButton.classList.toggle('active');
+});
+
+// Menutup navbar otomatis ketika salah satu menu link di dalamnya di-klik
+const navLinks = document.querySelectorAll('#navbar-menu a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbarMenu.classList.remove('active');
+        menuButton.classList.remove('active');
+    });
 });
